49a7294ed0d2064c5fc8abc73af7dff0508b3c3d
AlejxD <110071140+AlejxD@users.noreply.github.com>
2024-03-06 18:58:17 -0500

    Update entregable.js

--- entregable.js
+++ entregable.js
@@ -5,10 +5,10 @@ const arreglo3 = () => [true, false, true, false];
 const arreglo4 = () => [10, 20, 30, 40, 50];
 
 // Aplicar métodos de arreglos a los arreglos creados
-const resultado1 = arreglo1().map(num => num * 2); // Utilizando el método map para multiplicar cada elemento por 2
-const resultado2 = arreglo2().filter(letra => letra !== 'a'); // Utilizando el método filter para filtrar elementos diferentes de 'a'
-const resultado3 = arreglo3().every(valor => valor === true); // Utilizando el método every para verificar si todos los elementos son verdaderos
-const resultado4 = arreglo4().reduce((total, num) => total + num, 0); // Utilizando el método reduce para sumar todos los elementos
+const resultado1 = arreglo1().map(num => num * 2); 
+const resultado2 = arreglo2().filter(letra => letra !== 'a'); 
+const resultado3 = arreglo3().every(valor => valor === true); 
+const resultado4 = arreglo4().reduce((total, num) => total + num, 0); 
 
 // Mostrar resultados
 console.log("Resultado del método 1:", resultado1);
